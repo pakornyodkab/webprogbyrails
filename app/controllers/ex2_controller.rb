d90267ca2b503ab5d66f69a0da17524006a3b5d9
pakornyodkab <yodpakorn2543@gmail.com>
2021-09-01 21:52:21 +0700

    add ex2 loop into web

---
+++ app/controllers/ex2_controller.rb
@@ -0,0 +1,22 @@
+class Ex2Controller < ApplicationController
+  def loop
+    input1 = params[:a].to_s #if user are not give a input1 = " "
+    input2 = params[:b].to_s #if user are not give b input2 = " "
+    puts input1
+    puts input2
+    @a_notint = false
+    @b_notint = false
+    #check that a,b is int or not 
+    if (!input1.match?(/\A-?\d+\Z/)) # input1.match?(/\A-?\d+\Z/) this will return true if input1 is a number 
+      @a_notint = true
+    end
+
+    if (!input2.match?(/\A-?\d+\Z/))
+      @b_notint = true
+    end
+    # if user input a,b is not int or not input a,b when pass fn to_i it will give value to 0
+    @a= input1.to_i
+    @b = input2.to_i
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'loop' ,to:"ex2#loop"
   get 'welcome' ,to:"main#welcome"
   get 'contact_us',to:"main#contact"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
